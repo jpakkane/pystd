e3502843921832c377b4d60457b36136f9fa5be5
Jussi Pakkanen <jpakkane@gmail.com>
2025-06-08 12:28:30 +0300

    Only support static libraries for now.

--- src/pystd2025.cpp
+++ src/pystd2025.cpp
@@ -13,10 +13,15 @@
 #include <sys/mman.h>
 #endif
 
+/*
+ * These two are needed when building a shared library but not
+ * with a static one. For now support only static.
+ */
+/*
 void *operator new(size_t count) { return malloc(count); }
 
 void operator delete(void *ptr) noexcept { free(ptr); }
-
+*/
 void *operator new(size_t, void *ptr) noexcept { return ptr; }
 
 namespace pystd2025 {
