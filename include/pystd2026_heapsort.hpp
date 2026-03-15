c8c621be3202d823fd46d9581d66cad323be3a78
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2026-03-15 13:58:29 +0200

    Add comparator to heapsort.

--- include/pystd2026_heapsort.hpp
+++ include/pystd2026_heapsort.hpp
@@ -54,46 +54,54 @@ template<BasicIterator It> struct HeapInfo {
     }
 };
 
-template<BasicIterator It> void heapify(const HeapInfo<It> &hi, It loc) {
+template<BasicIterator It, typename Comparator>
+void heapify(const HeapInfo<It> &hi, It loc, const Comparator &cmp) {
     pystd2026::Optional<It> left = hi.left_child(loc);
     auto right = hi.right_child(loc);
     It largest = loc;
-    if(left && *left > *loc) {
+    if(left && cmp.compare(*left, *loc) > 0) {
         largest = left.value();
     }
-    if(right && *right > *largest) {
+    if(right && cmp.compare(*right, *largest) > 0) {
         largest = right.value();
     }
 
     if(largest != loc) {
         pystd2026::swap(*largest, *loc);
-        heapify(hi, largest);
+        heapify(hi, largest, cmp);
     }
 }
 
-template<BasicIterator It> void build_heap(const HeapInfo<It> &hi) {
+template<BasicIterator It, typename Comparator>
+void build_heap(const HeapInfo<It> &hi, const Comparator &cmp) {
     int64_t i = hi.size() / 2 - 1;
     for(; i >= 0; --i) {
-        heapify(hi, hi.begin + i);
+        heapify(hi, hi.begin + i, cmp);
     }
 }
 
-template<BasicIterator It> void heapsort(It begin, It end) {
+template<BasicIterator It, typename Comparator>
+void heapsort(It begin, It end, const Comparator &cmp) {
     const size_t MIN_SIZE = 16;
     if((size_t)(end - begin) <= MIN_SIZE) {
-        pystd2026::insertion_sort(begin, end);
+        pystd2026::insertion_sort(begin, end, cmp);
         return;
     }
     const HeapInfo original_heap{begin, end};
 
-    build_heap(original_heap);
+    build_heap(original_heap, cmp);
     // original_heap.validate_heap();
     for(size_t i = original_heap.size() - 1; i > MIN_SIZE; --i) {
         pystd2026::swap(*original_heap.begin, *(original_heap.begin + i));
         HeapInfo<It> shrunk_heap{begin, begin + i};
-        heapify(shrunk_heap, begin);
+        heapify(shrunk_heap, begin, cmp);
     }
     pystd2026::insertion_sort(begin, begin + MIN_SIZE + 1);
+}
+
+template<BasicIterator It> void heapsort(It begin, It end) {
+    using ValueType = pystd2026::remove_reference_t<decltype(*begin)>;
+    heapsort(begin, end, DefaultComparator<ValueType>{});
 }
 
 template<WellBehaved T> void heapsort(pystd2026::Span<T> span) {
