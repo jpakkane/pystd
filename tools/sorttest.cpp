517cacacb24b588bccf980122f755edd2ec6d1a2
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2026-03-18 12:48:59 +0200

    Add perf test apps.

---
+++ tools/sorttest.cpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 Jussi Pakkanen
+
+#include <pystd2026_introsort.hpp>
+
+// Test sorting N size_t's from a file.
+// The input data is assumed to have the
+// numbers 0 to N in a random order.
+
+int main() {
+    FILE *f = fopen("random.dat", "rb");
+    fseek(f, 0, SEEK_END);
+    const auto s = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    const size_t NUM_ENTRIES = s / sizeof(size_t);
+
+    printf("Sorting %d numbers.\n", (int)NUM_ENTRIES);
+
+    pystd2026::Vector<size_t> nums;
+    nums.reserve(NUM_ENTRIES);
+    for(size_t i = 0; i < NUM_ENTRIES; ++i) {
+        nums.push_back(0);
+    }
+    const auto ignore = fread(nums.data(), sizeof(size_t), NUM_ENTRIES, f);
+    (void)ignore;
+    fclose(f);
+
+    pystd2026::introsort(nums.begin(), nums.end());
+    for(size_t i = 0; i < NUM_ENTRIES; ++i) {
+        if(nums[i] != i) {
+            printf("Sort error!\n");
+            return 1;
+        }
+    }
+
+    return 0;
+}
---
+++ tools/stablesorttest.cpp
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 Jussi Pakkanen
+
+#include <pystd2026_mergesort.hpp>
+
+// Same as sorttest, but uses stable sort.
+
+int main() {
+    FILE *f = fopen("random.dat", "rb");
+    fseek(f, 0, SEEK_END);
+    const auto s = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    const size_t NUM_ENTRIES = s / sizeof(size_t);
+
+    printf("Sorting %d numbers.\n", (int)NUM_ENTRIES);
+
+    pystd2026::Vector<size_t> nums;
+    nums.reserve(NUM_ENTRIES);
+    for(size_t i = 0; i < NUM_ENTRIES; ++i) {
+        nums.push_back(0);
+    }
+    const auto ignore = fread(nums.data(), sizeof(size_t), NUM_ENTRIES, f);
+    (void)ignore;
+    fclose(f);
+
+    pystd2026::mergesort(nums.begin(), nums.end());
+    for(size_t i = 0; i < NUM_ENTRIES; ++i) {
+        if(nums[i] != i) {
+            printf("Sort error!\n");
+            return 1;
+        }
+    }
+
+    return 0;
+}
