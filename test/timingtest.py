3b4f1618d2cf1e97bbc06ae121a7b233211be800
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2026-05-18 13:56:40 +0300

    Refactor timing test to use on all headers.

--- test/timingtest.py
+++ test/timingtest.py
@@ -16,25 +16,11 @@ def get_std(options):
             return o['value']
     sys.exit('C++ std value could not be determined.')
 
-def test_unixy():
-    compilers = json.loads(open('meson-info/intro-compilers.json', 'r').read())
-    options = json.loads(open('meson-info/intro-buildoptions.json', 'r').read())
-    mi = json.loads(open('meson-info/meson-info.json', 'r').read())
-    cpp = compilers['build']['cpp']['exelist']
-    std_version = get_std(options)
-    std_flags = [f'-std={std_version}'] if std_version else []
+slow_headers = []
 
-    builddir = mi['directories']['build']
-    sourcedir = mi['directories']['source']
-    build_to_src = os.path.relpath(sourcedir, builddir)
-
-    compile_cmd = cpp + std_flags + ['-nostdinc++',
-                                     '-Iinclude',
-                                     '-I' + os.path.join(build_to_src, 'include'),
-                                     '-c',
-                                     '-o',
-                                     '/dev/null',
-                                     os.path.join(build_to_src, 'test/timingtest.cpp')]
+def test_header(compile_command_base, fname):
+    global slow_headers
+    compile_cmd = compile_command_base + [fname]
 
     fastest_time = 100
     num_tries = 5
@@ -49,8 +35,37 @@ def test_unixy():
             fastest_time = runtime
         if fastest_time < MAX_RUNTIME:
             return
-    sys.exit(f'Build time test took too long: {fastest_time}.')
+    slow_headers.append((fname, fastest_time))
 
+
+def test_unixy():
+    global slow_headers
+    compilers = json.loads(open('meson-info/intro-compilers.json', 'r').read())
+    options = json.loads(open('meson-info/intro-buildoptions.json', 'r').read())
+    mi = json.loads(open('meson-info/meson-info.json', 'r').read())
+    cpp = compilers['build']['cpp']['exelist']
+    std_version = get_std(options)
+    std_flags = [f'-std={std_version}'] if std_version else []
+
+    builddir = mi['directories']['build']
+    sourcedir = mi['directories']['source']
+    build_to_src = os.path.relpath(sourcedir, builddir)
+
+    fname = os.path.join(build_to_src, 'test/timingtest.cpp')
+    compile_command_base = cpp + std_flags + ['-nostdinc++',
+                                              '-Iinclude',
+                                              '-I' + os.path.join(build_to_src, 'include'),
+                                              '-c',
+                                              '-o',
+                                              '/dev/null']
+
+    test_header(compile_command_base, fname)
+
+    if slow_headers:
+        print('Slow headers:\n')
+        for fname, time in slow_headers:
+            print(time, fname)
+        sys.exit(1)
 
 if __name__ == "__main__":
     if sys.platform == 'win32':
