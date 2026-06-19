22c5d9e82dbe26ccf70665d765d125e3c10be501
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2026-06-19 19:47:25 +0300

    Parallelize header testing because it took 1s, which is way too long.

--- test/timingtest.py
+++ test/timingtest.py
@@ -7,6 +7,7 @@
 
 import os, sys, subprocess, shutil, json, time
 from glob import glob
+import concurrent.futures
 
 # GCC gets by in 0.1 seconds, but clang needs more.
 MAX_RUNTIME = 0.15
@@ -17,10 +18,7 @@ def get_std(options):
             return o['value']
     sys.exit('C++ std value could not be determined.')
 
-slow_headers = []
-
 def test_header(compile_command_base, fname):
-    global slow_headers
     compile_cmd = compile_command_base + [fname]
 
     fastest_time = 100
@@ -35,8 +33,8 @@ def test_header(compile_command_base, fname):
         if runtime < fastest_time:
             fastest_time = runtime
         if fastest_time < MAX_RUNTIME:
-            return
-    slow_headers.append((fname, fastest_time))
+            return None
+    return (fname, fastest_time)
 
 def get_header_list(source_root):
     headers = []
@@ -46,8 +44,13 @@ def get_header_list(source_root):
     assert len(headers) > 0
     return headers
 
+def test_one_header(fname, contents, include_name, compile_command_base):
+    open(fname, 'w').write(contents.replace('PYSTD_HEADER', include_name))
+    retval = test_header(compile_command_base, fname)
+    os.unlink(fname)
+    return retval
+
 def test_unixy():
-    global slow_headers
     compilers = json.loads(open('meson-info/intro-compilers.json', 'r').read())
     options = json.loads(open('meson-info/intro-buildoptions.json', 'r').read())
     mi = json.loads(open('meson-info/meson-info.json', 'r').read())
@@ -59,7 +62,6 @@ def test_unixy():
     sourcedir = mi['directories']['source']
     build_to_src = os.path.relpath(sourcedir, builddir)
 
-    fname = os.path.join(build_to_src, 'test/timingtest.cpp')
     fname_in = os.path.join(build_to_src, 'test/timingtest.cpp.in')
     compile_command_base = cpp + std_flags + ['-nostdinc++',
                                               '-Iinclude',
@@ -70,10 +72,23 @@ def test_unixy():
 
     contents = open(fname_in, 'r').read()
 
-    for header_name in get_header_list(sourcedir):
-        open(fname, 'w').write(contents.replace('PYSTD_HEADER', header_name))
-        test_header(compile_command_base, fname)
-        os.unlink(fname)
+    slow_headers = []
+    num_subprocesses = os.process_cpu_count()
+    if num_subprocesses > 1:
+        num_subprocesses -= 1
+    futures = []
+
+    i = 0
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_subprocesses) as executor:
+        for header_name in get_header_list(sourcedir):
+            fname = os.path.join(build_to_src, f'test/timingtest{i}.cpp')
+            f = executor.submit(test_one_header, fname, contents, header_name, compile_command_base)
+            futures.append(f)
+            i += 1
+        for f in futures:
+            res = f.result()
+            if res is not None:
+                slow_headers.append(res)
 
     if slow_headers:
         print('Slow headers:\n')
